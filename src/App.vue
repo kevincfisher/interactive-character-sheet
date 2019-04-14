57562da842b5e621252f6f43e4a45fe9a1c319a7
Kevin Fisher <kfisher@listrak.com>
2019-04-14 19:47:47 -0400

    added some basic components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Wrapper>
+        <CharacterSheet/>
+    </Wrapper>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Wrapper from './components/Wrapper.vue'
+import CharacterSheet from './components/CharacterSheet.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Wrapper,
+    CharacterSheet
   }
 }
 </script>
@@ -23,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/CharacterSheet.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="level">
+        Character sheet goes here
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'CharacterSheet'
+};
+</script>
+
+<style></style>
